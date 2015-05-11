c092c2aafd1512d16b8ebae607a7497e7511f6ea
marlon neila seron <seronmns@gmail.com>
2015-05-10 21:53:08 -0300

    Adding new alias.

--- bash/alias.sh
+++ bash/alias.sh
@@ -2,16 +2,27 @@
 alias reload="source ~/.bash_profile"
 
 # List
-alias ll='ls -lahG'
+alias ll="ls -lahG"
 
 # Processes
-alias tu='top -o cpu'
-alias tm='top -o vsize'
+alias tu="top -o cpu"
+alias tm="top -o vsize"
 alias k9="killall -9"
 
 # Misc
 alias tlsys="tail -f /var/log/system.log"
 
+# Tools
+alias g="git"
+alias vg="vagrant"
+
+# Ruby
+alias r="rails"
+alias rs="rails s -b 0.0.0.0"
+alias rk="rake"
+alias bi="bundle install"
+
+# Functions
 function cdd(){
   cd ~/Developments/$*
 }
