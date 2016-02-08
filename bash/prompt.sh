704a9ac6f05cc6e4177ccb106212500667d551a6
marlon neila seron <seronmns@gmail.com>
2016-02-08 17:31:13 -0200

    Change prompt colors

--- bash/prompt.sh
+++ bash/prompt.sh
@@ -9,30 +9,16 @@ __my_prompt () {
 
   local NO_COLOR="\[\e[0m\]"
 
-  local BLACK="\[\033[0;30m\]"
-  local BLUE="\[\033[0;34m\]"
-  local CYAN="\[\033[0;36m\]"
-  local GRAY="\[\033[1;30m\]"
-  local GREEN="\[\033[0;32m\]"
-  local RED="\[\033[0;31m\]"
-  local VIOLET="\[\033[0;35m\]"
-  local WHITE="\[\033[0;37m\]"
-  local YELLOW="\[\033[0;33m\]"
-
-  local LIGHT_BLACK="\[\033[1;30m\]"
-  local LIGHT_BLUE="\[\033[1;34m\]"
-  local LIGHT_CYAN="\[\033[1;36m\]"
-  local LIGHT_GRAY="\[\033[0;37m\]"
-  local LIGHT_GREEN="\[\033[1;32m\]"
-  local LIGHT_RED="\[\033[1;31m\]"
-  local LIGHT_VIOLET="\[\033[1;35m\]"
-  local LIGHT_WHITE="\[\033[1;37m\]"
-  local LIGHT_YELLOW="\[\033[1;33m\]"
+  local INFO_SYMBOL="\342\201\231"
+  local PROMPT_SYMBOL="\342\235\257"
+  local MAGENTA="\e[38;5;141m"
+  local DARK_BLUE="\e[38;5;75m"
+  local LIGHT_BLUE="\e[38;5;51m"
 
   if [ "$GIT_BRANCH" != "" ]; then
-      PS1="\n$YELLOW[\u] $RED[${RUBY_VERSION}] $GREEN\w $CYAN[${GIT_BRANCH}]${NO_COLOR}\n\$ "
+      PS1="\n$INFO_SYMBOL $MAGENTA\u $DARK_BLUE\W $LIGHT_BLUE${GIT_BRANCH} ${NO_COLOR}\n$PROMPT_SYMBOL "
   else
-      PS1="\n$YELLOW[\u] $RED[${RUBY_VERSION}] $GREEN\w${NO_COLOR}\n\$ "
+      PS1="\n$INFO_SYMBOL $MAGENTA\u $DARK_BLUE\W ${NO_COLOR}\n$PROMPT_SYMBOL "
   fi
 }
 
